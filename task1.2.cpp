0e96fbfa77ab86b50bdc56f9bc0a4e1fd026aa53
jindastrialman <keklolich@gmail.com>
2021-12-07 20:01:43 +0300

    задание 2

--- task1.2.cpp
+++ task1.2.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 
+float matrix_mult(float ** matrix, int size)
+{
+	float result = 1;
+
+	for(int i = 0; i < size/2; i++)
+	{
+		for(int j = i + 1; j < size - i - 1; j++)
+		{
+			if(matrix[i][j] != 0)
+				result *= matrix[i][j];
+			if(matrix[size - i - 1][j] != 0)
+				result *= matrix[size - i - 1][j];
+		}
+
+	}
+	return result;
+}
+
+using namespace std;
+
 int main()
 {
+	int n; // порядок матрицы
+	float **matrix; // матрица
 
+	setlocale(LC_ALL, "russian");
+
+
+	cout << "Введите порядок матрицы: ";
+	cin >> n;
+
+	matrix = new float*[n];
+	for(int i = 0; i < n; i++)
+		matrix[i] = new float[n];// выделение памяти под массив
+
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			cin >> matrix[i][j];
+
+	cout << "Если умножить элементы, то получится : " << matrix_mult(matrix, n);
+
+	for(int i = 0; i < n; i++)
+	{
+		delete [] matrix[i]; // отчищаем память
+	}
+	delete [] matrix;
 }
